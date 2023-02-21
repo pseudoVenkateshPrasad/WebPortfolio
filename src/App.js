b6ba85a943950806f9a2595e53bb711ff1c82091
Venkatesh Kotipalli <venkatesh.prasad2096@gmail.com>
2023-02-21 20:33:33 +0530

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import Homepage from './components/Homepage';
 
 function App() {
   return (
     <div className="App">
       <h1>App Component</h1>
+      <div>
+        <Homepage />
+      </div>
     </div>
   );
 }
